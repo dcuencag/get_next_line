d3efe97888d7822dee23c9be1012ba57e435730a
Daniel Cuenca <d@loscuenca.com>
2025-02-20 15:56:00 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,152 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dancuenc <dancuenc@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 13:42:42 by dancuenc          #+#    #+#             */
+/*   Updated: 2025/02/20 15:52:24 by dancuenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include "get_next_line_utils.c"
+
+char	*read_and_update_stash(int fd, char *storage)
+{
+	char	*buff;
+	int		bytes_read;
+	char	*tmp;
+
+	bytes_read = 1;
+	if (!storage)
+		storage = ft_calloc(sizeof(char), 1);
+	if (!storage)
+		return (NULL);
+	buff = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	while (!ft_strchr(buff, '\n') && bytes_read != 0)
+	{
+		bytes_read = read(fd, buff, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free(buff), free(storage), NULL);
+		buff[bytes_read] = '\0';
+		tmp = storage;
+		storage = ft_strjoin(storage, buff);
+		free(tmp);
+	}
+	free(buff);
+	return (storage);
+}
+
+char	*extract_line(char *current_line)
+{
+	char	*new_line;
+	int		i;
+
+	i = 0;
+	if (!current_line)
+		return (NULL);
+	if (current_line && (!ft_strchr(current_line, '\n')))
+		return (current_line);
+	new_line = ft_calloc(sizeof(char), ft_strlen(current_line) + 2);
+	if (!new_line)
+		return (NULL);
+	while (current_line[i] != '\n' && current_line[i] != '\0')
+	{
+		new_line[i] = current_line[i];
+		i++;
+	}
+	new_line[i] = current_line[i];
+	new_line[i + 1] = '\0';
+	return (new_line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash = NULL;
+	char		*line_read;
+	char		*tmp;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	stash = read_and_update_stash(fd, stash);
+	if (ft_strchr(stash, '\n') || !ft_strchr(stash, '\0'))
+	{
+		line_read = extract_line(stash);
+		if (!ft_strchr(stash, '\n'))
+		{
+			line_read = stash;
+			stash = NULL;
+			return (line_read);
+		}
+		//sacar esta parte y quitar strchr para poder tener solo 5 funciones en utils
+		tmp = stash;
+		stash = ft_substr(stash, ft_strchr(stash, '\n') - stash + 1, ft_strlen(stash));
+		free(tmp);
+		return (line_read);
+	}
+	if (stash && stash[0] != '\0')
+	{
+		line_read = stash;
+		stash = NULL;
+		return (line_read);
+	}
+	return (NULL);
+}
+
+int main()
+{
+	int fd = open("try.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Failed to open file.\n");
+		return 1;
+	}
+/* 
+	printf("%s", get_next_line(fd));
+ 	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd)); */
+ 	char *line;
+//	int i = 0;
+//	line = get_next_line(fd);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+/* 		line = get_next_line(fd);
+		i++; */
+		printf("%s", line);
+	}
+	close(fd);
+	return 0;
+}
---
+++ get_next_line.h
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dancuenc <dancuenc@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 13:48:44 by dancuenc          #+#    #+#             */
+/*   Updated: 2025/02/20 15:10:06 by dancuenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+
+char	*get_next_line(int fd);
+
+/**
+ * Concatenate two strings
+ * @param s1 The first string
+ * @param s2 The second string
+ * @return A pointer to the concatenated string
+ */
+char	*ft_strjoin(char const *s1, char const *s2);
+
+/**
+ * Find the first occurrence of a character in a string
+ * 
+ * @param s The string to search
+ * @param c The character to find
+ * @return A pointer to the first occurrence of the character in the string, or NULL if not found
+ */
+char	*ft_strchr(const char *s, int c);
+
+/**
+ * Duplicate a string
+ * @param s1 The string to duplicate
+ * @return A pointer to the duplicated string
+ */
+char	*ft_strdup(const char *s);
+
+/**
+ * Calculate the length of a string
+ * @param str The string to calculate the length of
+ * @return The length of the string
+ */
+size_t	ft_strlen(const char *s);
+
+/**
+ * Extract a substring from a string
+ * @param s The string to extract from
+ * @param start The starting index of the substring
+ * @param len The length of the substring
+ * @return A pointer to the extracted substring
+ */
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
