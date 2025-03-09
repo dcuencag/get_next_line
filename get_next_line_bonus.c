786c80ab7f2c7b99ca40745035590180285c4574
Daniel Cuenca <d@loscuenca.com>
2025-03-09 12:02:21 +0100

    Add files via upload

---
+++ get_next_line_bonus.c
@@ -0,0 +1,202 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dancuenc <dancuenc@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 12:42:19 by dancuenc          #+#    #+#             */
+/*   Updated: 2025/03/09 12:00:29 by dancuenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strcpy(char *src)
+{
+	char	*dst;
+	size_t	i;
+
+	if (!src)
+		return (NULL);
+
+	dst = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	free(src);
+	return (dst);
+}
+
+/**
+ * Read from a file descriptor until a newline character is found
+ * @param fd The file descriptor to read from
+ * @param storage The string to store the read data
+ * @return A pointer to the stored data, or NULL if an error occurred
+ */
+char	*read_to_stash(int fd, char *storage)
+{
+	char	*buff;
+	int		bytes_read;
+	char	*tmp;
+
+	bytes_read = 1;
+	if (!storage)
+		storage = ft_calloc(sizeof(char), 1);
+	if (!storage)
+		return (NULL);
+	buff = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	if (!buff)
+		return (free(storage), NULL);
+	while (!ft_strchr(buff, '\n') && bytes_read != 0)
+	{
+		bytes_read = read(fd, buff, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free(buff), free(storage), NULL);
+		buff[bytes_read] = '\0';
+		tmp = ft_strjoin(storage, buff);
+		if (!tmp)
+			return (free(buff), free(storage), write(2, "Error: malloc\n", 15), NULL);
+		storage = ft_strcpy(tmp);
+	}
+	free(buff);
+	return (storage);
+}
+
+/**
+ * Extract a line from a string
+ * @param current_line The string to extract the line from
+ * @return A pointer to the extracted line, or NULL if an error occurred
+ */
+char	*extract_line(char *current_line)
+{
+	char	*new_line;
+	int		i;
+
+	if (!current_line)
+		return (NULL);
+	if (current_line && (!ft_strchr(current_line, '\n')))
+	{
+		return (current_line);
+	}
+	new_line = ft_calloc(sizeof(char), ft_strlen(current_line) + 2);
+	if (!new_line)
+		return (NULL);
+	i = 0;
+	while (current_line[i] != '\n' && current_line[i] != '\0')
+	{
+		new_line[i] = current_line[i];
+		i++;
+	}
+	if (current_line[i] == '\n')
+		new_line[i] = current_line[i];
+	new_line[i + 1] = '\0';
+	return (new_line);
+}
+
+/**
+ * Update the stash after a line has been extracted
+ * @param stash The string to update
+ * @return A pointer to the updated stash
+ */
+char	*update_stash(char **stash)
+{
+	char	*line_read;
+	char	*tmp;
+	char	*ptr;
+
+	if (!stash)
+		return (NULL);
+	line_read = extract_line(*stash);
+	if (!line_read)
+		return (NULL);
+	if (!ft_strchr(*stash, '\n'))
+	{
+		return (ft_strcpy(*stash));
+	}
+	tmp = ft_strchr(*stash, '\n') + 1;
+	if (!tmp)
+		return (NULL);
+	ptr = ft_substr(tmp, 0, ft_strlen(tmp));
+	tmp = NULL;
+	free(*stash);
+	*stash = ptr;
+	return (line_read);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash[4096];
+	char		*line;
+
+	if (fd <= 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash[fd] = read_to_stash(fd, stash[fd]);
+	if (!stash[fd])
+		return (NULL);
+	if (ft_strchr(stash[fd], '\n'))
+		return (update_stash(&stash[fd]));
+	line = ft_strcpy(stash[fd]);
+	if (line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	stash[fd] = NULL;
+	return (line);
+}
+/* 
+int	main(int ac, char **av)
+{
+	int fd;
+	int fd2;
+	int fd3;
+	char *line;
+
+
+	fd = open(av[1], O_RDONLY);
+	fd2 = open(av[2], O_RDONLY);
+	fd3 = open(av[3], O_RDONLY);
+
+	while (fd != 0 || fd2 != 0 || fd3 != 0)
+	{
+		line = get_next_line(fd);
+		if (fd != 0 && line)
+		{
+			printf("%s", line);
+			free(line);
+		}
+		else if (fd != 0)
+		{
+			close(fd);
+			fd = 0;
+		}
+		line = get_next_line(fd2);
+		if (fd2 != 0 && line)
+		{
+			printf("%s", line);
+			free(line);
+		}
+		else if (fd2 != 0)
+		{
+			close(fd2);
+			fd2 = 0;
+		}
+		line = get_next_line(fd3);
+		if (fd3 != 0 && line)
+		{
+			printf("%s", line);
+			free(line);
+		}
+		else if (fd3 != 0)
+		{
+			close(fd3);
+			fd3 = 0;
+		}
+	}
+	return (0);
+} */
