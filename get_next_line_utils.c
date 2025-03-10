56355e68dc1b676a856fd21d01f2504b5851a4d3
Daniel Cuenca <d@loscuenca.com>
2025-03-10 18:13:47 +0100

    GNL evaluado 125/100
    GNL terminado y evaluado por tres estudiantes y moulinette

---
+++ get_next_line_utils.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dancuenc <dancuenc@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 13:48:41 by dancuenc          #+#    #+#             */
+/*   Updated: 2025/03/08 13:33:34 by dancuenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	while (i < count * size)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *stash, char *buffer)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		total;
+
+	total = ft_strlen(stash) + ft_strlen(buffer);
+	str = ft_calloc(sizeof(char), (total + 1));
+	if (!str || !stash || !buffer)
+		return (NULL);
+	i = 0;
+	while (stash[i])
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	j = 0;
+	while (buffer[j])
+	{
+		str[i] = buffer[j];
+		i++;
+		j++;
+	}
+	if (stash)
+		free(stash);
+	return (str);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	if (!s || start >= ft_strlen(s))
+		return (NULL);
+	sub = (char *)ft_calloc(sizeof(char), len + 1);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!str || (ft_strlen(str) == 1 && str[0] == '\0'))
+		return (NULL);
+	if (c == '\0')
+		return (str);
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	return (NULL);
+}
